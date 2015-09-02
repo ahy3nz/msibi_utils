053263dbcc9964359e8f26ae02cbc6771d304dda
Tim Moore <tcmoore3@gmail.com>
2015-09-02 15:50:41 -0500

    Move the find_fits() to own function parse_logfile
    Since parsing this logfile will be used for more than just plotting the
    fits, it makes sense to have it be a stand-alone function

--- msibi_utils/plot_fit.py
+++ msibi_utils/plot_fit.py
@@ -17,26 +17,6 @@ def plot_pair_fits(pair, fits, use_agg=False):
     fig.savefig('figures/%s-fit.pdf' % pair)
     plt.close('all')
 
-def find_fits(filename): 
-    # errs is a dict with keys 'type1-type2' for the pairs
-    # the values are dicts, with the keys the state name and the values the 
-    # list of fit values at that state
-    fits = {}
-    for line in open(filename, 'r'):
-        try: 
-            keyword = line.split()[1]
-        except IndexError:
-            pass
-        if keyword == 'pair':
-            try:
-                fits[line.split()[2][:-1]][line.split()[4][:-1]].append(line.split()[-1])
-            except KeyError:  # pair not in fits
-                try:
-                    fits[line.split()[2][:-1]][line.split()[4][:-1]] = [line.split()[-1]]
-                except KeyError:  # state not in pairs in fits
-                    fits[line.split()[2][:-1]] = {line.split()[4][:-1]: [line.split()[-1]]}
-    return fits
-
 def plot_all(filename, use_agg=False):
     """Plot fitness function vs. iteration for each pair at each state
 
