1b6278366634e89cf11aa160a9ddd38da4fdcec5
Tim Moore <tcmoore3@gmail.com>
2015-09-02 13:30:18 -0500

    gitignore and setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 
 requirements = [line.strip() for line in open('requirements.txt').readlines()]
-setup(name='msibi',
+setup(name='msibi_utils',
       version='0.1',
       description='',
       url='http://github.com/tcmoore3/msibi_utils',
