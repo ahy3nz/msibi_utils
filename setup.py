d9e0a13eb85d333fad8a4a866af0e4286b329b92
Tim Moore <tcmoore3@gmail.com>
2015-09-02 13:07:38 -0500

    First commit

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+
+requirements = [line.strip() for line in open('requirements.txt').readlines()]
+setup(name='msibi',
+      version='0.1',
+      description='',
+      url='http://github.com/tcmoore3/msibi_utils',
+      author='Timothy C. Moore',
+      author_email='timothy.c.moore@vanderbilt.edu',
+      license='MIT',
+      packages=['msibi_utils'],
+      install_requires=requirements,
+      zip_safe=False)
